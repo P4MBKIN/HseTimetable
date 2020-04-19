f5739bd25f267c6172c77a58fd873ba72c718cda
Pavel Antonov <paxant@mail.ru>
2020-04-19 08:03:34 +0300

    Initial Commit

---
+++ HseTimetable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HseTimetable
+//
+//  Created by Pavel on 19.04.2020.
+//  Copyright © 2020 Hse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
